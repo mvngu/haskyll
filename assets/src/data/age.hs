cee152fdc352fa03705f33449f4e1e6f97f16f43
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-11-30 15:03:32 +1100

    data: Tabby's age

---
+++ assets/src/data/age.hs
@@ -0,0 +1,6 @@
+-- How old Tabby will be in x years.
+main = do
+    let age = (1 :: Integer)
+    let x = (3 :: Integer)
+    putStrLn ("Tabby's age: " ++ show age)
+    putStrLn ("In 3 years time, Tabby will be: " ++ show (age + x))
