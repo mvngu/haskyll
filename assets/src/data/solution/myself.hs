a77dba4bd31ef6493c6130e94e8a755a57fcbbbf
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-11-30 19:59:50 +1100

    data: solution to an exercise

---
+++ assets/src/data/solution/myself.hs
@@ -0,0 +1,8 @@
+-- My personal information.
+main = do
+    let name = "Sam"
+    let age = (23 :: Integer)
+    let hobby = "anime connoisseur"
+    putStrLn ("My name is " ++ name ++ ".")
+    putStrLn ("I'm " ++ show age ++ " years old.")
+    putStrLn ("I'm an " ++ hobby ++ ".")
