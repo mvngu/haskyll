52efcb0c1a715f744e0a08a73eb6f01f1db56d9e
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-02 07:24:18 +1100

    data: solution to Mercury exercise

---
+++ assets/src/data/solution/mercury.hs
@@ -0,0 +1,8 @@
+-- Convert age from Earth years to Mercury years.
+main = do
+    putStrLn "Enter your age in years."
+    year <- getLine
+    let y = (read year :: Double)
+    let earthDay = 365 * y
+    let mercuryYear = round (earthDay / 88)
+    putStrLn ("Your age on Mercury: " ++ show mercuryYear ++ " years")
