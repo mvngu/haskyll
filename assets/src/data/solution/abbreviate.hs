d4cfd62cec26d3c38ed19734a076ea1319c60d08
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-04 07:26:19 +1100

    data: solution to exercise on abbreviation

---
+++ assets/src/data/solution/abbreviate.hs
@@ -0,0 +1,11 @@
+import           Data.Char
+import           Text.Printf
+
+-- Abbreviate a two-word phrase.
+main = do
+    putStrLn "Enter a two-word phrase."
+    phrase <- getLine
+    let [first, second] = words phrase
+    let left = toUpper (head first)
+    let right = toUpper (head second)
+    putStrLn (printf "%s is abbreviated as %c%c" phrase left right)
