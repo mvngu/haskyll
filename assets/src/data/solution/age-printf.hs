1a3b57c7b7244d7311a741757df28117b238f11d
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-03 20:14:52 +1100

    data: rewrite some scripts to use `printf` for string formatting

---
+++ assets/src/data/solution/age-printf.hs
@@ -0,0 +1,9 @@
+import Text.Printf
+
+-- How old Tabby will be in x years.
+-- Use the function printf for string formatting.
+main = do
+    let age = (1 :: Integer)
+    let x = (3 :: Integer)
+    putStrLn (printf "Tabby's age: %d" age)
+    putStrLn (printf "In 3 years time, Tabby will be: %d" (age + x))
---
+++ assets/src/data/solution/circle-printf.hs
@@ -0,0 +1,10 @@
+import Text.Printf
+
+-- Calculate the area of a circle.
+-- Use the function printf for string formatting.
+main = do
+    putStrLn "Please enter the radius."
+    radius <- getLine
+    let r = (read radius :: Double)
+    let area = pi * (r ** 2)
+    putStrLn (printf "Area of circle: %f" area)
