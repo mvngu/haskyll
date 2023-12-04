fd275a8b7469fe96f2dd5e7ed81d0865e706c0d0
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-05 04:20:37 +1100

    data: solution to exercise on pet preferences

---
+++ assets/src/data/solution/pet.hs
@@ -0,0 +1,10 @@
+import           Data.Bits
+import           Text.Printf
+
+-- Sam's pet preferences.
+main = do
+    let cat = True
+    let dog = False
+    printf "Sam likes cats and dogs? %s\n" $ show $ cat && dog
+    printf "Sam likes cats or dogs? %s\n" $ show $ cat || dog
+    printf "Sam likes cats XOR dogs? %s\n" $ show $ xor cat dog
