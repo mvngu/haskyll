1bba55685057478a3fb4e000b75ea74a4bafe72d
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-04 13:13:55 +1100

    data: solution to exercise on pruning a string

---
+++ assets/src/data/solution/prune.hs
@@ -0,0 +1,9 @@
+-- Remove the character at index i in a string.
+main = do
+    putStrLn "Enter a string."
+    str <- getLine
+    putStrLn "Enter an integer."
+    index <- getLine
+    let k = (read index :: Int)
+    let (left, right) = splitAt (k + 1) str
+    putStrLn ((init left) ++ right)
