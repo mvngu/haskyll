f9cfb95e0c64eb42bdcb770caafe25685cc2e8e3
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-04 20:06:04 +1100

    data: examples of boolean `&&`

---
+++ assets/src/data/and.hs
@@ -0,0 +1,11 @@
+import           Text.Printf
+
+-- Boolean and.
+main = do
+    let coffee = True
+    let tea = False
+    let water = True
+    let beer = False
+    putStrLn (printf "Coffee and tea? %s" (show (coffee && tea)))
+    putStrLn (printf "Coffee and water? %s" (show (coffee && water)))
+    putStrLn (printf "Tea and beer? %s" (show (tea && beer)))
