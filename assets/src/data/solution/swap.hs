340cf9e0f5c4e29958d1b7d6f2e17d37dda08a3f
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-04 13:00:50 +1100

    data: solution to exercise on swapping first and last characters

---
+++ assets/src/data/solution/swap.hs
@@ -0,0 +1,10 @@
+import           Text.Printf
+
+-- Swap the first and last characters of a string with each other.
+main = do
+    putStrLn "Enter a string."
+    str <- getLine
+    let front = head str : ""
+    let end = last str : ""
+    let mid = init (tail str)
+    putStrLn (printf "%s%s%s" end mid front)
