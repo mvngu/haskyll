2e0b75cfd0ce1b4b7e6b99a595032d20b09c51e8
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 07:25:33 +1100

    decide: solution to exercise on maximum of integers

---
+++ assets/src/decide/solution/max.hs
@@ -0,0 +1,15 @@
+import           Text.Printf
+
+-- The maximum of two integers.
+main = do
+    putStrLn "Enter first integer."
+    a <- getLine
+    putStrLn "Enter second integer."
+    b <- getLine
+    let k = (read a :: Integer)
+    let n = (read b :: Integer)
+    let max =
+            if k > n
+                then k
+                else n
+    printf "Maximum of %s and %s: %d\n" a b max
