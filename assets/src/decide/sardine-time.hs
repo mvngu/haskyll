8c8e4ba678350fc6c808192ed88ae3ab95e5ca90
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 05:42:11 +1100

    decide: example of conditional expression

---
+++ assets/src/decide/sardine-time.hs
@@ -0,0 +1,13 @@
+import           Text.Printf
+
+-- The day and time for sardine.
+main = do
+    let today = "Friday"
+    let sardineDay = "Friday"
+    let time = 1300 -- 1:00 pm in 24-hour time
+    let sardineTime = 1300
+    let meal =
+            if (today == sardineDay) && (time == sardineTime)
+                then "sardine"
+                else "other scheduled meal"
+    printf "Today's meal: %s\n" meal
