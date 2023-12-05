f7cbac552e65d378b690df14b89f65fa6b05e2e0
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 05:55:01 +1100

    decide: solution to exercise on days of the week

---
+++ assets/src/decide/solution/weekend.hs
@@ -0,0 +1,11 @@
+import           Text.Printf
+
+-- Is it weekend yet?
+main = do
+    putStrLn "Enter a day of the week."
+    day <- getLine
+    let period =
+            if (day == "Saturday") || (day == "Sunday")
+                then "weekend"
+                else "weekday"
+    printf "%s is a %s\n" day period
