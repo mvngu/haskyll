48822d7c82ff73c61ba1538122e32ceeca64b67c
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 07:26:07 +1100

    decide: example of nested conditional expression

---
+++ assets/src/decide/nested.hs
@@ -0,0 +1,17 @@
+import           Text.Printf
+
+-- Relationship between two integers.
+main = do
+    putStrLn "Enter first integer."
+    a <- getLine
+    putStrLn "Enter second integer."
+    b <- getLine
+    let k = (read a :: Integer)
+    let n = (read b :: Integer)
+    let relation =
+            if k < n
+                then "less than"
+                else if k > n
+                         then "greater than"
+                         else "equal to"
+    printf "%s is %s %s\n" a relation b
