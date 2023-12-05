b6e5a421cad37993c2a43f9a041e64bc8065aef8
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-05 18:14:00 +1100

    decide: program to decide whether it's sardine day

---
+++ assets/src/decide/sardine.hs
@@ -0,0 +1,11 @@
+import           Text.Printf
+
+-- Sardine day.
+main = do
+    let today = "Friday"
+    let sardineDay = "Friday"
+    let meal =
+            if today == sardineDay
+                then "sardine"
+                else "other scheduled meal"
+    printf "Today's meal: %s\n" meal
