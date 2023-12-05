35bdfa8f32b465f1cc37f3a7b6b02590f93b7ae9
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-05 14:51:19 +1100

    data: incomplete program for exercise on comparison

---
+++ assets/src/data/quiz.hs
@@ -0,0 +1,8 @@
+import           Text.Printf
+
+-- How many correct solutions?
+main = do
+    let resultA = 2 < 3
+    let resultB = 37 /= (read "37" :: Integer)
+    let resultC = 2 == 2.0
+    -- Insert code here.
