d7c36ed71cc08172e7eb98d49e675ac6daa25a0c
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-05 13:59:42 +1100

    data: program for a small dictionary

---
+++ assets/src/data/dictionary.hs
@@ -0,0 +1,12 @@
+import           Text.Printf
+
+-- A small dictionary.
+main = do
+    let a = "aardvark"
+    let b = "armadillo"
+    let c = "aardwolf"
+    let d = "Aardvark"
+    printf "b < a? %s\n" $ show $ b < a
+    printf "c < a? %s\n" $ show $ c < a
+    printf "b < c? %s\n" $ show $ b < c
+    printf "a < d? %s\n" $ show $ a < d
