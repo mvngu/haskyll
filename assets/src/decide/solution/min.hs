fe4fc8a6edbb9fd5c98b52eca2b7168ca77ae993
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 07:36:13 +1100

    decide: solution to exercise on minimum of integers

---
+++ assets/src/decide/solution/min.hs
@@ -0,0 +1,15 @@
+import           Text.Printf
+
+-- The minimum of two integers.
+main = do
+    putStrLn "Enter first integer."
+    a <- getLine
+    putStrLn "Enter second integer."
+    b <- getLine
+    let k = (read a :: Integer)
+    let n = (read b :: Integer)
+    let min =
+            if k < n
+                then k
+                else n
+    printf "Minimum of %s and %s: %d\n" a b min
