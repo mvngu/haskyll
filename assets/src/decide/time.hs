763fb01579fb9d572cf303394a941fd7fefc96cb
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 16:25:37 +1100

    decide: program to obtain the current time

---
+++ assets/src/decide/time.hs
@@ -0,0 +1,13 @@
+import           Data.Time
+import           Text.Printf
+
+-- Current time of the day in 24-hour format.
+main = do
+    dateTime <- getCurrentTime
+    timeZone <- getCurrentTimeZone
+    let localDateTime = utcToLocalTime timeZone dateTime
+    let (TimeOfDay hour minute second) = localTimeOfDay localDateTime
+    let h = toInteger hour
+    let m = toInteger minute
+    let s = floor second :: Integer
+    printf "Current time: %d hours, %d minutes, and %d seconds\n" h m s
