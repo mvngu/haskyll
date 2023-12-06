98e15f15cc4f486df5e1d32d90e8e30e95d318f6
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 17:48:48 +1100

    decide: solution to exercise on high noon

---
+++ assets/src/decide/solution/noon.hs
@@ -0,0 +1,15 @@
+import           Data.Time
+import           Text.Printf
+
+-- Is it noon yet?
+main = do
+    dateTime <- getCurrentTime
+    timeZone <- getCurrentTimeZone
+    let localDateTime = utcToLocalTime timeZone dateTime
+    let (TimeOfDay hour _ _) = localTimeOfDay localDateTime
+    let h = toInteger hour
+    let time =
+            if h == 12
+                then "It's high noon"
+                else "Not yet high noon"
+    putStrLn time
