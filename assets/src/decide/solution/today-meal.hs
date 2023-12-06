9aa2c5c1daca3419a7b0fe6b3c4fc49369e06bfb
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 14:37:10 +1100

    decide: solution to exercise on today's meal

---
+++ assets/src/decide/solution/today-meal.hs
@@ -0,0 +1,17 @@
+import           Data.Time
+import           Text.Printf
+
+-- Tabby's meal for today.
+main = do
+    time <- getCurrentTime
+    let (year, month, day) = toGregorian $ utctDay time
+    let today = show $ dayOfWeek $ fromGregorian year month day
+    let porkDay = "Monday"
+    let sardineDay = "Friday"
+    let meal =
+            if today == porkDay
+                then "pork and mixed vegetables"
+                else if today == sardineDay
+                         then "sardine"
+                         else "other scheduled meal"
+    printf "Today's meal: %s\n" meal
