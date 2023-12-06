7015ab8098db9b2eaf4d8cb53df8e75865b47ebb
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 14:27:33 +1100

    decide: program to print current day of the week

---
+++ assets/src/decide/day.hs
@@ -0,0 +1,9 @@
+import           Data.Time
+import           Text.Printf
+
+-- The current day of the week.
+main = do
+    time <- getCurrentTime
+    let (year, month, day) = toGregorian $ utctDay time
+    let today = dayOfWeek $ fromGregorian year month day
+    printf "Today is %s\n" $ show today
