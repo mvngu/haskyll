38610b878a067b78fa3f98a56df3086e032bc39f
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 17:58:29 +1100

    decide: solution to exercise on integer parity

---
+++ assets/src/decide/solution/parity.hs
@@ -0,0 +1,12 @@
+import           Text.Printf
+
+-- Whether an integer is even or odd.
+main = do
+    putStrLn "Enter an integer."
+    num <- getLine
+    let n = read num :: Integer
+    let parity =
+            if (mod n 2) == 0
+                then "even"
+                else "odd"
+    printf "%d is %s\n" n parity
