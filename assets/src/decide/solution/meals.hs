e327624a0c770dce001f85c5b22cd32642ccb76e
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-06 12:47:18 +1100

    decide: solution to exercise on Tabby's meals

---
+++ assets/src/decide/solution/meals.hs
@@ -0,0 +1,14 @@
+import           Text.Printf
+
+-- Tabby's meals on Monday and Friday.
+main = do
+    let today = "Monday"
+    let porkDay = "Monday"
+    let sardineDay = "Friday"
+    let meal =
+            if today == porkDay
+                then "pork and mixed vegetables"
+                else if today == sardineDay
+                         then "sardine"
+                         else "other scheduled meal"
+    printf "Today's meal: %s\n" meal
