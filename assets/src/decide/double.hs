0c57f01145d8bc03dd85ca6ace7d8d662d3e170e
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-07 11:33:46 +1100

    decide: example of `do` block in `if/then/else`

---
+++ assets/src/decide/double.hs
@@ -0,0 +1,12 @@
+import           Text.Printf
+
+-- Double an integer.
+main = do
+    putStrLn "Enter an integer."
+    num <- getLine
+    let n = read num :: Integer
+    if n < 0
+        then putStrLn "I'm positive the number is negative"
+        else do
+            let nn = 2 * n
+            printf "It's Dublin: %d\n" nn
