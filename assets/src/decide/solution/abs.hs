8c489cfc6be82d1776ceebd3aa4d7da1846ba220
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-07 14:31:20 +1100

    decide: solution to exercise on absolute value

---
+++ assets/src/decide/solution/abs.hs
@@ -0,0 +1,12 @@
+import           Text.Printf
+
+-- The absolute value of a number.
+main = do
+    putStrLn "Enter a number."
+    num <- getLine
+    let n = read num :: Double
+    if n >= 0
+        then printf "Absolute value = %s\n" $ show n
+        else do
+            let abs = n * (-1)
+            printf "Absolute value = %s\n" $ show abs
