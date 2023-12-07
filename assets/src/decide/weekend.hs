e324549abf1650f78e28b78dc97dab0e3da6e67c
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-07 12:54:21 +1100

    decide: todo lists for weekend

---
+++ assets/src/decide/weekend.hs
@@ -0,0 +1,16 @@
+import           Text.Printf
+
+-- Todo lists for the weekend.
+main = do
+    putStrLn "Enter a day."
+    day <- getLine
+    if day == "Saturday"
+        then do
+            putStrLn "1. Grocery shopping"
+            putStrLn "2. Stock up on sardine and tuna cans"
+        else if day == "Sunday"
+                 then do
+                     putStrLn "1. Load the washing machine"
+                     putStrLn "2. Comb and brush Tabby"
+                     putStrLn "3. Hang the clothes out to dry"
+                 else putStrLn "Not the weekend"
