0255ef82dac6bd7e6f7873799dee9861e8aadc76
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-09 08:21:11 +1100

    decide: solution to exercise on properties of a circle

---
+++ assets/src/decide/solution/circle.hs
@@ -0,0 +1,22 @@
+import Text.Printf
+
+-- The area of a circle.
+area :: Double -> Double
+area x = pi * (x ** 2)
+
+-- The circumference of a circle.
+circumference :: Double -> Double
+circumference x = pi * (diameter x)
+
+-- The diameter of a circle.
+diameter :: Double -> Double
+diameter x = 2 * x
+
+main = do
+    putStrLn "Enter the radius of a circle."
+    radius <- getLine
+    let r = read radius :: Double
+    printf "Radius: %f\n" r
+    printf "Diameter: %f\n" $ diameter r
+    printf "Circumference: %f\n" $ circumference r
+    printf "Area: %f\n" $ area r
