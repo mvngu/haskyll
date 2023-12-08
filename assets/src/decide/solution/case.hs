68944c299eab87748e6b1f2ee28b9ba1c4b96a4c
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-08 15:58:00 +1100

    decide: solution to exercise on reversing the case of a string

---
+++ assets/src/decide/solution/case.hs
@@ -0,0 +1,19 @@
+import Data.Char
+import Text.Printf
+
+-- Reverse the case of a string.
+flipCase :: String -> String
+flipCase str =
+    if isLower $ head str
+        then (toUpper $ head str) : tail str
+        else (toLower $ head str) : tail str
+
+main = do
+    let a = "miss Honey"
+    let b = "waltzing Matilda"
+    let c = "Banana"
+    let d = "23 skidoo"
+    printf "%s -> %s\n" a $ flipCase a
+    printf "%s -> %s\n" b $ flipCase b
+    printf "%s -> %s\n" c $ flipCase c
+    printf "%s -> %s\n" d $ flipCase d
