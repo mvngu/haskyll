0f7ba939afe914ff5a65c74667605bbab3d9e3f6
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-08 17:47:34 +1100

    decide: solution to exercise on temperature conversion

---
+++ assets/src/decide/solution/temperature.hs
@@ -0,0 +1,15 @@
+import Text.Printf
+
+-- Convert a temperature value from Fahrenheit to Celsius.
+toCelsius :: Double -> Double
+toCelsius x = (x - 32.0) / 1.8
+
+-- Convert a temperature value from Celsius to Fahrenheit.
+toFahrenheit :: Double -> Double
+toFahrenheit x = (x * 1.8) + 32.0
+
+main = do
+    let c = 37 :: Double
+    let f = 212 :: Double
+    printf "%f Celsius -> %f Fahrenheit\n" c $ toFahrenheit c
+    printf "%f Fahrenheit -> %f Celsius\n" f $ toCelsius f
