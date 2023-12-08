30ba4af64d05048d7327150544f7a3ac7172711b
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-08 17:10:38 +1100

    decide: solution to exercise on triangular numbers

---
+++ assets/src/decide/solution/triangular-number.hs
@@ -0,0 +1,15 @@
+import Text.Printf
+
+-- The triangular number.
+trinum :: Integer -> Integer
+trinum x = div (x * (x + 1)) 2
+
+main = do
+    let a = 1 :: Integer
+    let b = 2 :: Integer
+    let c = 3 :: Integer
+    let d = 100 :: Integer
+    printf "%d -> %d\n" a $ trinum a
+    printf "%d -> %d\n" b $ trinum b
+    printf "%d -> %d\n" c $ trinum c
+    printf "%d -> %d\n" d $ trinum d
