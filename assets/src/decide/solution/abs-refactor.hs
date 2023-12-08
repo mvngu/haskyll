4885ca628bd0e7eaf87e853df0b7216fa7a93dfd
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-09 08:32:55 +1100

    decide: solution to exercise on refactoring absolute value

---
+++ assets/src/decide/solution/abs-refactor.hs
@@ -0,0 +1,14 @@
+import Text.Printf
+
+-- The absolute value of a number.
+absolute :: Double -> Double
+absolute x =
+    if x >= 0
+        then x
+        else -x
+
+main = do
+    putStrLn "Enter a number."
+    num <- getLine
+    let n = read num :: Double
+    printf "Absolute value = %f\n" $ absolute n
