4496d5ea0b72f94f7aeabc8e2f46998fc723b688
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-09 09:38:12 +1100

    decide: solution to exercise on removing middle character(s) of string

---
+++ assets/src/decide/solution/drop-middle.hs
@@ -0,0 +1,13 @@
+-- Remove the middle character(s) of a string.
+dropMid :: String -> String
+dropMid str = do
+    let k = length str
+    let (left, right) = splitAt (div k 2) str
+    if (mod k 2) == 0
+        then (init left) ++ (tail right)
+        else left ++ (tail right)
+
+main = do
+    putStrLn "Enter a string."
+    str <- getLine
+    putStrLn $ dropMid str
