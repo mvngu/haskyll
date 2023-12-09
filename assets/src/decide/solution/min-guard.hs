4d5021da4f13ba0c01d21f85424f209dc01b69ad
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-10 08:57:20 +1100

    decide: solution to exercise on minimum of integers

---
+++ assets/src/decide/solution/min-guard.hs
@@ -0,0 +1,15 @@
+import Text.Printf
+
+-- | The minimum of two integers.
+minInt :: Integer -> Integer -> Integer
+minInt x y
+    | x < y = x
+    | otherwise = y
+
+main = do
+    let a = 1 :: Integer
+    let b = 2 :: Integer
+    let c = 3 :: Integer
+    printf "Min of %d and %d: %d\n" a b $ minInt a b
+    printf "Min of %d and %d: %d\n" (-b) c $ minInt (-b) c
+    printf "Min of %d and %d: %d\n" c c $ minInt c c
