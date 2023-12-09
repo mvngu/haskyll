50005076f071f33ca04fbdd19da1ffd5f68bbd58
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-10 09:07:21 +1100

    decide: solution to exercise on relation between two integers

---
+++ assets/src/decide/solution/nested-guard.hs
@@ -0,0 +1,17 @@
+import Text.Printf
+
+-- Relationship between two integers.
+relation :: Integer -> Integer -> String
+relation a b
+    | a < b = "less than"
+    | a > b = "greater than"
+    | otherwise = "equal to"
+
+main = do
+    putStrLn "Enter first integer."
+    a <- getLine
+    putStrLn "Enter second integer."
+    b <- getLine
+    let k = read a :: Integer
+    let n = read b :: Integer
+    printf "%s is %s %s\n" a (relation k n) b
