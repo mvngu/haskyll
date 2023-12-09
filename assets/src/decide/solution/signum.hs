7dabc20a10f806faad588fbe3de1118ee980d8f5
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-10 09:17:24 +1100

    decide: solution to exercise on signum function

---
+++ assets/src/decide/solution/signum.hs
@@ -0,0 +1,16 @@
+import Text.Printf
+
+-- The signum function.
+sgn :: Double -> Integer
+sgn x
+    | x < 0 = -1 :: Integer
+    | x == 0 = 0 :: Integer
+    | x > 0 = 1 :: Integer
+
+main = do
+    let a = -5
+    let b = 0
+    let c = 42
+    printf "%f -> %d\n" a $ sgn a
+    printf "%f -> %d\n" b $ sgn b
+    printf "%f -> %d\n" c $ sgn c
