6cf4102eed58a40834badc0a82fd6141c6a7fce5
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-09 22:46:11 +1100

    decide: cup sizes corresponding to coffee drinks

---
+++ assets/src/decide/coffee.hs
@@ -0,0 +1,15 @@
+import Text.Printf
+
+-- The cup size corresponding to a coffee beverage.
+cupSize :: String -> String
+cupSize drink
+    | drink == "espresso" = "56--85 ml"
+    | drink == "cappuccino" = "142--170 ml"
+    | drink == "latte" = "227--426 ml"
+    | drink == "frappuccino" = "312--426 ml"
+    | otherwise = "unknown drink"
+
+main = do
+    putStrLn "Enter a coffee drink."
+    drink <- getLine
+    printf "%s -> %s\n" drink $ cupSize drink
