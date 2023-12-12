55e508336f0319a455d2222d3b00146094afecd6
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-12 18:44:19 +1100

    decide: reimplement negation as pattern matching

---
+++ assets/src/decide/not.hs
@@ -0,0 +1,12 @@
+import Text.Printf
+
+-- | Reimplementation of the library function "not".
+myNot :: Bool -> Bool
+myNot True  = False
+myNot False = True
+
+main = do
+    let test1 = (not True) == (myNot True)
+    let test2 = (not False) == (myNot False)
+    printf "%s -> %s, check: %s\n" (show True) (show $ myNot True) $ show test1
+    printf "%s -> %s, check: %s\n" (show False) (show $ myNot False) $ show test2
