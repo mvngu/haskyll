233586d6fb789e2aa216ce5f877347ce33291674
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-13 14:26:59 +1100

    decide: reimplement the function `head`

---
+++ assets/src/decide/first-rest.hs
@@ -0,0 +1,18 @@
+import Text.Printf
+
+-- | An implementation of the function "fst" for lists.
+first :: [a] -> a
+first []     = error "Empty list"
+first (x:xs) = x
+
+main = do
+    let a = 1 :: Integer
+    let b = 2 :: Integer
+    let c = 3 :: Integer
+    let x = 'x'
+    let y = 'y'
+    let z = 'z'
+    printf "[%d, %d] -> %d\n" a b $ first [a, b]
+    printf "[%d, %d, %d] -> %d\n" a b c $ first [a, b, c]
+    printf "[%c, %c] -> %c\n" x y $ first [x, y]
+    printf "[%c, %c, %c] -> %c\n" x y z $ first [x, y, z]
