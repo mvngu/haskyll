4be01c6b2e8a55fa7351006a3998870e14c5ad74
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-13 16:50:29 +1100

    decide: merge traffic code into one file

--- assets/src/decide/traffic.hs
+++ assets/src/decide/traffic.hs
@@ -8,12 +8,25 @@ trafficg light
     | light == "green" = "go"
     | otherwise = "Invalid traffic light"
 
+-- Meaning of each traffic light.
+trafficp :: String -> String
+trafficp "red"    = "stop"
+trafficp "orange" = "wait"
+trafficp "green"  = "go"
+trafficp _        = "Invalid traffic light"
+
 main = do
     let a = "red"
     let b = "orange"
     let c = "green"
     let d = "purple"
+    -- Guarded equation.
     printf "%s -> %s\n" a $ trafficg a
     printf "%s -> %s\n" b $ trafficg b
     printf "%s -> %s\n" c $ trafficg c
     printf "%s -> %s\n" d $ trafficg d
+    -- Pattern matching.
+    printf "%s -> %s\n" a $ trafficp a
+    printf "%s -> %s\n" b $ trafficp b
+    printf "%s -> %s\n" c $ trafficp c
+    printf "%s -> %s\n" d $ trafficp d
