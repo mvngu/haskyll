955db0ac1ff8dd7894b63e323041372d65f0d84b
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-19 07:38:51 +1100

    recurse: solution to exercise on lambda expression to half integer

---
+++ assets/src/recurse/solution/half.hs
@@ -0,0 +1,38 @@
+--------------------------------------------------------------------------------
+-- MIT License
+--
+-- Copyright (C) 2023 Duck McSouls <quacksouls [AT] gmail [DOT] com>
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--------------------------------------------------------------------------------
+
+import Text.Printf
+
+-- | A lambda expression that halves an integer if it is even, otherwise add 1
+-- to the integer.
+main = do
+    let n = [1 .. 10] :: [Integer]
+    let half =
+            map
+                (\x ->
+                     if (mod x 2) == 0
+                         then div x 2
+                         else x + 1)
+                n
+    printf "%s -> %s\n" (show n) $ show half
