27ee39f1df680ef1a49fd5df3990f58f2e7e3596
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-18 16:27:48 +1100

    recurse: solution to exercise on integer parity

---
+++ assets/src/recurse/solution/parity.hs
@@ -0,0 +1,37 @@
+--------------------------------------------------------------------------------
+-- MIT License
+--
+-- Copyright (C) 2023 Duck McSouls <quacksouls [AT] gmail [DOT] com>
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--------------------------------------------------------------------------------
+
+import Text.Printf
+
+-- | A list of the first n positive even integers.
+evenList :: Integral a => a -> [a]
+evenList n = map (\x -> 2 * x) [1 .. n]
+
+-- | A list of the first n positive odd integers.
+oddList :: Integral a => a -> [a]
+oddList n = map (\x -> 2 * x + 1) [0 .. n - 1]
+
+main = do
+    printf "%s\n" $ show $ evenList 10
+    printf "%s\n" $ show $ oddList 10
