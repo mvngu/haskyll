ed8f8aecbd7f57725ea9b658b0bc918e3a84de6e
quacksouls <66396308+quacksouls@users.noreply.github.com>
2024-01-01 12:55:09 +1100

    list: solution to exercise on Champernowne constant

---
+++ assets/src/list/solution/champernowne.hs
@@ -0,0 +1,42 @@
+--------------------------------------------------------------------------------
+-- MIT License
+--
+-- Copyright (C) 2024 Duck McSouls <quacksouls [AT] gmail [DOT] com>
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--------------------------------------------------------------------------------
+
+import System.Random.Stateful
+import Text.Printf
+
+-- | A partial Champernowne constant.
+champ :: Integer -> String
+champ n
+    | n < 1 = error "Must be positive integer"
+    | otherwise = "0." ++ join [1 .. n]
+
+-- | Join the list elements into a string.
+join :: (Show a, Num a) => [a] -> String
+join []     = ""
+join (x:xs) = foldl (\acc y -> acc ++ (show y)) (show x) xs
+
+-- | A partial Champernowne constant.
+main = do
+    n <- uniformRM (1, 100) globalStdGen :: IO Integer
+    printf "%s\n" $ show . champ $ n
