2a8f3efa5c5b2fbf3ef3df33d6a5313d0fdb8f50
quacksouls <66396308+quacksouls@users.noreply.github.com>
2024-01-02 03:41:30 +1100

    list: examples on wildcard in list comprehension

---
+++ assets/src/list/first.hs
@@ -0,0 +1,47 @@
+--------------------------------------------------------------------------------
+-- MIT License
+--
+-- Copyright (C) 2024 Duck McSouls <quacksouls [AT] gmail [DOT] com>
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--------------------------------------------------------------------------------
+
+import Text.Printf
+
+-- | The first elements in a list of 2-tuples.
+firstA :: [(a, b)] -> [a]
+firstA xs = [x | (x, _) <- xs]
+
+-- | The second elements in a list of 2-tuples.
+secondA :: [(a, b)] -> [b]
+secondA xs = [y | (_, y) <- xs]
+
+-- | The elements of a list of 2-tuples.
+main = do
+    let a = [(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'e')]
+    let b = [(x, y) | x <- [1 .. 5], y <- [6 .. 10]]
+    let c = [(a, b) | a <- "abcdef", b <- "ghij"]
+    putStrLn "First elements"
+    printf "%s -> %s\n" (show a) $ show . firstA $ a
+    printf "%s -> %s\n" (show b) $ show . firstA $ b
+    printf "%s -> %s\n" (show c) $ show . firstA $ c
+    putStrLn "\nSecond elements"
+    printf "%s -> %s\n" (show a) $ show . secondA $ a
+    printf "%s -> %s\n" (show b) $ show . secondA $ b
+    printf "%s -> %s\n" (show c) $ show . secondA $ c
